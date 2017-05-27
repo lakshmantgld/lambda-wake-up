0163a571f09e88650b17f8afd3a53b610879daf9
lakshman <diwaakartg@gmail.com>
2017-05-27 13:31:26 +0900

    annonymized request URL and options

--- handler.js
+++ handler.js
@@ -8,10 +8,10 @@ module.exports.wakeup = (event, context, callback) => {
   console.log("calling every one minute");
 
   var options = {
-    "query":"mutation{createProduct(product: { mainTitle: \"Test\", title: \"Check\", flavor: \"fff\", price: \"30\", sku: \"ffff\", sizes: [small], nicotine_strengths: [two, four], images: [{ purpose: \"ard\", url: \"ddd\"}, {purpose: \"large\", url: \"ddssdds\"}], routeTag: \"dfsdfsdfs\", vendor: \"dfdsfsdf\", blurb: \"sdsdf\"}) {_id, product { mainTitle title flavor price sku sizes nicotine_strengths routeTag vendor blurb images{ purpose url } dates {added_to_store  removed_from_store }}}}"
+    "query":"some query"
   }
 
-  request.post('https://xefwksxwta.execute-api.ap-northeast-1.amazonaws.com/development/graphql' , { body: JSON.stringify(options)}, function (err, response, body) {
+  request.post('https://url-to-wakeup' , { body: JSON.stringify(options)}, function (err, response, body) {
     if (err) {
       return console.error('waking up failed:', err);
     }
